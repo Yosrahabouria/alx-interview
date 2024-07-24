2ad99efbf953d46b99046d724461ff536da47b58
Yosrahabouria <yosrahabouria@gmail.com>
2024-07-24 05:54:25 -0700

    second commit

---
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+Validate utf-8
+"""
+
+def validUTF8(data):
+    count = 0
+    first_bit = 1 << 7
+    second_bit = 1 << 6
+
+    for num in data:
+        current_bit = 1 << 7
+
+        if count == 0:
+            while current_bit & num:
+                count += 1
+                current_bit >>= 1
+
+            if count == 0:
+                continue
+
+            if count == 1 or count > 4:
+                return False
+
+        else:
+            if not (num & first_bit and not (num & second_bit)):
+                return False
+
+        count -= 1
+
+    return count == 0
