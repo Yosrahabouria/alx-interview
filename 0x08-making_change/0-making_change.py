f5abe531e5f37b65f4f55cc5884ff929957c6865
Yosrahabouria <yosrahabouria@gmail.com>
2024-08-20 13:30:47 -0700

    commit

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+defines the function make_Change
+"""
+
+
+def makeChange(coins, total):
+    """
+    make change function 
+    """
+
+    if total <= 0:
+        return 0
+
+    number_coins = 0
+    coins.sort(reverse=True)
+
+    for coin in coins:
+        while coin <= total:
+            total -= coin
+            number_coins += 1
+
+    if total == 0:
+        return number_coins
+    return -1
